98befb64169f7c1242574e7d05bf8f21a0ea5e52
TaegyunWoo <dnxprbs@gmail.com>
2021-02-13 15:34:29 +0900

    Finish LockedPage (but, NO Ajax)

--- LockedPageJS.js
+++ LockedPageJS.js
@@ -1,5 +1,13 @@
 window.onload = function() {
-   
+   //확인 버튼 (로그인) 이벤트
+   var confirmBtn = document.getElementById("confirm");
+   confirmBtn.addEventListener("click", LoginEvent);
+   function LoginEvent(e) {
+        var pw = document.getElementById("pwField").value;
+        if(pw == "1533dlek") {
+            open("ChatRoomsLobby.html");
+        }
+   }
     
 
 };
