2a5a8c6380f0a1ea2e732ca9b4e53012798339e5
TaegyunWoo <dnxprbs@gmail.com>
2021-02-17 13:12:16 +0900

    Change How to Login

--- LockedPageJS.js
+++ LockedPageJS.js
@@ -1,33 +1,40 @@
-//json파일 가져오는 함수
-function getUserInfoJson() {
-    var req = new XMLHttpRequest();
-    var userInfo;
+window.onload = function() {
 
-    req.onreadystatechange = function() {
-        if(req.readyState == 4) {
-            if(req.status == 200) {
-                userInfo = JSON.parse(req.responseText);
-                return userInfo;
-            }
-        }
-    };
+    //json파일 가져오는 함수
+    function getUserInfoJson() {
+        var req = new XMLHttpRequest();
+        var userInfo;
     
-    req.open("GET", "userInfo.json", false);
-    req.send(null);
-}
-
-//잠금모드 로그인 이벤트 처리 (only 비밀번호)
-var pwField = document.getElementById("pwField");
-var confirmBtn = document.getElementById("confirm");
-
-confirmBtn.addEventListener("click", lockedLoginEvent);
-
-//잠금모드 로그인 이벤트리스너
-function lockedLoginEvent(e) {
-    var pwValue = pwField.value;
-    var userInfo = getUserInfoJson();
-    if(pwValue == userInfo.password) {
-        location.replace("ChatRoomsLobby.html");
+        req.onreadystatechange = function() {
+            if(req.readyState == 4) {
+                if(req.status == 200) {
+                    userInfo = JSON.parse(req.responseText);
+                    return userInfo;
+                }
+            }
+        };
+        
+        req.open("GET", "userInfo.json", false);
+        req.send(null);
     }
     
-}
+    //잠금모드 로그인 이벤트 처리 (only 비밀번호)
+    var pwField = document.getElementById("pwField");
+    var confirmBtn = document.querySelector("form > input#confirm");
+    console.log(confirmBtn);
+    confirmBtn.addEventListener("click", lockedLoginEvent);
+    
+    //잠금모드 로그인 이벤트리스너
+    function lockedLoginEvent(e) {
+        e.preventDefault();
+        var pwValue = pwField.value;
+        var userInfo = getUserInfoJson();
+        if(pwValue == userInfo.password) {
+            location.replace("ChatRoomsLobby.html");
+        } else {
+            alert("비밀번호를 확인해주세요.");
+        }
+        
+    }
+
+};
