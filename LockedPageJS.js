8536619933d3e87548cc35ada19cab709a001b8d
TaegyunWoo <dnxprbs@gmail.com>
2021-02-17 12:44:40 +0900

    Change Btn to Submit

--- LockedPageJS.js
+++ LockedPageJS.js
@@ -1,35 +1,33 @@
-window.onload = function() {
-    //json파일 가져오는 함수
-    function getUserInfoJson() {
-        var req = new XMLHttpRequest();
-        var userInfo;
+//json파일 가져오는 함수
+function getUserInfoJson() {
+    var req = new XMLHttpRequest();
+    var userInfo;
 
-        req.onreadystatechange = function() {
-            if(req.readyState == 4) {
-                if(req.status == 200) {
-                    userInfo = req.response;
-                    console.log(`JSON: ${userInfo}`)
-                    return userInfo;
-                }
+    req.onreadystatechange = function() {
+        if(req.readyState == 4) {
+            if(req.status == 200) {
+                userInfo = JSON.parse(req.responseText);
+                return userInfo;
             }
-        };
-        
-        req.open("GET", "userInfo.json", false);
-        req.send(null);
-    }
-
-    //잠금모드 로그인 이벤트 처리 (only 비밀번호)
-    var pwField = document.getElementById("pwField");
-    var confirmBtn = document.getElementById("confirm");
-
-    confirmBtn.addEventListener("click", lockedLoginEvent);
+        }
+    };
     
-    //잠금모드 로그인 이벤트리스너
-    function lockedLoginEvent(e) {
-        var pwValue = pwField.value;
-        var userInfo = getUserInfoJson();
+    req.open("GET", "userInfo.json", false);
+    req.send(null);
+}
 
-        
+//잠금모드 로그인 이벤트 처리 (only 비밀번호)
+var pwField = document.getElementById("pwField");
+var confirmBtn = document.getElementById("confirm");
+
+confirmBtn.addEventListener("click", lockedLoginEvent);
+
+//잠금모드 로그인 이벤트리스너
+function lockedLoginEvent(e) {
+    var pwValue = pwField.value;
+    var userInfo = getUserInfoJson();
+    if(pwValue == userInfo.password) {
+        location.replace("ChatRoomsLobby.html");
     }
-
-};
+    
+}
