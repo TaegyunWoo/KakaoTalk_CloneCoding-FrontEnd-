4d096fa6b8a65e06c36d744c34a5a90670d751f7
TaegyunWoo <dnxprbs@gmail.com>
2021-02-18 22:01:40 +0900

    Doing find friend event

--- Main/FriendsLobbyEvent.js
+++ Main/FriendsLobbyEvent.js
@@ -4,7 +4,7 @@ function person(n, p, s) {
     this.stateMessage = s;
 
     //매개변수 = 추가할 페이지의 document객체
-    this.addFriend = function(Addhr) {
+    this.addFriend = function(addHr) {
         
         var friendProduct = document.createElement("div");
         var nameElement = document.createElement("span");
@@ -16,6 +16,7 @@ function person(n, p, s) {
         profileElement.setAttribute("src", this.profile);
         stateMessageElement.innerHTML = this.stateMessage;
         profileDiv.appendChild(profileElement);
+        nameElement.setAttribute("class", "name");
         
         
         friendProduct.appendChild(document.createElement("br"));
@@ -32,7 +33,7 @@ function person(n, p, s) {
         if(addHr) {
             friendProduct.appendChild(document.createElement("hr"));
         }
-
+        
     };
 
     //style설정
@@ -68,12 +69,39 @@ function person(n, p, s) {
     }
 }
 
+//친구 찾기
+function findFriend(name) {
+    var friends = document.getElementsByClassName("name");
+    var findedFriends = new Array();
+    for(var i=0; i<friends.length; i++) {
+        var reg = new RegExp(name);
+        if( !reg.test(friends[i].innerHTML) ) {
+            friends[i].style.display = "none";
+        }
+    }
+}
+
 window.onload = function() {
     //나 추가
     var me = new person("우태균", "../images/profile.png", "열심히 공부합시다!");
     me.addFriend(true);
     
+    //친구추가 이벤트
+    var addFriendElement = document.getElementsByClassName("addFriend")[0];
+    console.log(addFriendElement);
+    addFriendElement.addEventListener("click", addFriendEvent, false);
+    function addFriendEvent(e) {
+        var name = prompt("이름을 입력하세요.");
+        var profileImgNum = parseInt(prompt("이미지 번호를 입력하세요. (1~5)"));
+        var stateMessage = prompt("상태메시지를 입력하세요.");
+
+        var friend = new person(name, `../images/profile${profileImgNum}.png`, stateMessage);
+        friend.addFriend();
+    }
     
+    //친구찾기 검색바 생성
     
 
+    //친구찾기 검색바 제거
+    
 };
